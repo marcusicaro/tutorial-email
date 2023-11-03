d0f0abdede4568fd18cfe6bcb332c8cc72dea39b
Marcus <marcusicaromc@gmail.com>
2023-11-03 14:43:06 -0300

    remove try/catch

--- app.js
+++ app.js
@@ -24,79 +24,67 @@ app.use(bodyParser.json());
 app.post(
   '/create_account',
   asyncHandler(async (req, res) => {
-    try {
-      const verificationCode = Math.floor(Math.random() * 1000000);
-      const { username, email } = req.body;
+    const verificationCode = Math.floor(Math.random() * 1000000);
+    const { username, email } = req.body;
 
-      if (!validator.validate(email)) {
-        return res.status(400).send('Email inválido');
-      }
-      const userWithSameEmail = await User.findOne({ email: email });
-      if (userWithSameEmail) {
-        return res.status(400).send('Email já cadastrado');
-      }
-      if (typeof username !== 'string' || !username.length > 0) {
-        return res.status(400).send('Usuário inválido');
-      }
-      const userWithSameUsername = await User.findOne({ username: username });
-      if (userWithSameUsername) {
-        return res.status(400).send('Usuário já existe');
-      }
-
-      const user = new User({
-        username: username,
-        email: email,
-        verificationCode: verificationCode,
-      });
-
-      await user.save();
-      await sendEmail(email, verificationCode);
-      return res.status(201).send('Usuário criado com sucesso');
-    } catch (err) {
-      return res.status(500).send('Erro ao criar conta');
+    if (!validator.validate(email)) {
+      return res.status(400).send('Email inválido');
     }
+    const userWithSameEmail = await User.findOne({ email: email });
+    if (userWithSameEmail) {
+      return res.status(400).send('Email já cadastrado');
+    }
+    if (typeof username !== 'string' || !username.length > 0) {
+      return res.status(400).send('Usuário inválido');
+    }
+    const userWithSameUsername = await User.findOne({ username: username });
+    if (userWithSameUsername) {
+      return res.status(400).send('Usuário já existe');
+    }
+
+    const user = new User({
+      username: username,
+      email: email,
+      verificationCode: verificationCode,
+    });
+
+    await user.save();
+    await sendEmail(email, verificationCode);
+    return res.status(201).send('Usuário criado com sucesso');
   })
 );
 
 app.post(
   '/validate_user',
   asyncHandler(async (req, res) => {
-    try {
-      const { username, verificationCode } = req.body;
-      const user = await User.findOne({
-        username: username,
-        verificationCode: verificationCode,
-      });
-      if (!user) {
-        return res.status(400).send('Usuário não encontrado');
-      }
-      if (user.verified === true) {
-        return res.status(400).send('Usuário já validado');
-      }
-      user.verified = true;
-      await user.save();
-      return res.status(200).send('Usuário validado com sucesso');
-    } catch {
-      return res.status(500).send('Erro ao validar usuário');
+    const { username, verificationCode } = req.body;
+    const user = await User.findOne({
+      username: username,
+      verificationCode: verificationCode,
+    });
+    if (!user) {
+      return res.status(400).send('Usuário não encontrado');
     }
+    if (user.verified === true) {
+      return res.status(400).send('Usuário já validado');
+    }
+    user.verified = true;
+    await user.save();
+    return res.status(200).send('Usuário validado com sucesso');
   })
 );
 
 app.post(
   '/login',
   asyncHandler(async (req, res) => {
-    try {
-      const { username } = req.body;
-      const user = await User.findOne({
-        username: username,
-      });
-      if (user.verified === false) {
-        return res.status(400).send('Usuário não validado');
-      }
-      return res.send('Olá, usuário validado!');
-    } catch {
-      return res.status(500).send('Erro ao realizar login.');
+    const { username } = req.body;
+    const user = await User.findOne({
+      username: username,
+    });
+    if (user.verified === false) {
+      return res.status(400).send('Usuário não validado');
     }
+    return res.send('Olá, usuário validado!');
   })
 );
 
